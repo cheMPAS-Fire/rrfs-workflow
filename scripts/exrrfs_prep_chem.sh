4eba56148826971e6c33f51c6e08eb4de623f52a
Sudheer Bhimireddy <sudheer.r.bhimireddy@noaa.gov>
2026-07-13 12:12:39 -0600

    Comment out concatenation of previous_day with HH
    Comment out the line that concatenates previous_day with HH.

--- scripts/exrrfs_prep_chem.sh
+++ scripts/exrrfs_prep_chem.sh
@@ -58,7 +58,7 @@ current_hh="${HH}" #$(date -d "${HH}" +"%H")
 #
 #prev_hh=$(date -d "$current_hh -24 hour" +"%H")
 previous_day=$(date '+%C%y%m%d' -d "$current_day-1 days")
-previous_day="${previous_day} ${HH}"
+#previous_day="${previous_day} ${HH}"
 #
 if [[ ${DOW} -le 5 ]]; then
    DOW_STRING=weekdy
