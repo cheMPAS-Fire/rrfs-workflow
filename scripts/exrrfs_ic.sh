0ffc01c0ba8588c3de51c96c621e7a026a1540e3
jordanschnell <jordan.schnell@noaa.gov>
2026-09-09 15:09:50 +0000

    move chem_ic_update task to prep_ic to handle warm start cycles

--- scripts/exrrfs_ic.sh
+++ scripts/exrrfs_ic.sh
@@ -74,10 +74,6 @@ if [[ ! -s './init.nc' ]]; then
   err_exit
 fi
 
-# add/update chemistry species to init.nc
-if [[ "${DO_CHEMISTRY^^}" == "TRUE" ]]; then
-  source "${USHrrfs}"/chem_ic_update.sh
-fi
 
 # copy init.nc to COMOUT
 ${cpreq} "${DATA}/init.nc" "${COMOUT}/ic/${WGF}${MEMDIR}"
